163e416d88e992da0368fd8681954ce8a48b2491
RunarSmith <ewhat@protonmail.com>
2025-08-21 20:01:23 +0000

    [volund] update

--- volund.ps1
+++ volund.ps1
@@ -193,7 +193,7 @@ class Configuration {
             "labelVolumes" = "volund"
 
             "buildOpts" = "" # "--tls-verify=false"
-            "pullOpts" = ""  # "--tls-verify=false"
+            "pullOpts" = "--log-level debug"  # "--tls-verify=false"
 
             "sharedResourcesVolume" = @{
                 "name"      = "sharedResources"
